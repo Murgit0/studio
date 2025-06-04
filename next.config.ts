0d167398289d2550be4176c350ba66cb29d3ae08
SM Chuzza Mishra <smchuzza@gmail.com>
2025-06-04 16:00:21 +0000

    now just allow ALL image hostnames

--- next.config.ts
+++ next.config.ts
@@ -7,73 +7,12 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
+        hostname: '**', // Allow any hostname for HTTPS
       },
       {
-        protocol: 'https',
-        hostname: 'images.pexels.com',
-        port: '',
-        pathname: '/**',
+        protocol: 'http',
+        hostname: '**', // Allow any hostname for HTTP
       },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com', // Kept in case of future use
-        port: '',
-        pathname: '/**',
-      },
-      { 
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.googleusercontent.com', 
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'encrypted-tbn0.gstatic.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'newsapi.org', // For NewsAPI images (though often they point to other domains)
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'ichef.bbci.co.uk', // For BBC News images
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'static.politico.com', // For Politico images
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.cnn.com', // For CNN images
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.rpgsite.net', // For RPGSite images
-        port: '',
-        pathname: '/**',
-      }
-      // When integrating real image search from other services,
-      // or if NewsAPI returns images from other domains,
-      // you will need to add their image CDN hostnames here for next/image optimization.
     ],
   },
   typescript: {
@@ -82,5 +21,3 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
-
-    
