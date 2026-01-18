aa5ddf406fd33fe407ede6c3823eb356a54ed6c9
SM Chuzza Mishra <smchuzza@gmail.com>
2026-01-18 16:48:35 +0000

    Remove CSP immediately

--- next.config.ts
+++ next.config.ts
@@ -1,35 +1,8 @@
 
 import type {NextConfig} from 'next';
 
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data: https://*;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'none';
-    connect-src *;
-`.replace(/\s{2,}/g, ' ').trim();
-
-
 const nextConfig: NextConfig = {
   output: 'standalone', // Recommended for Netlify and similar platforms
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader,
-          },
-        ],
-      },
-    ]
-  },
   images: {
     remotePatterns: [
       {
